90c9b6f1f2f48b170767a47f06e8d7ec4add65c0
Andre Vale <andrevale_6@hotmail.com>
2024-05-15 02:52:06 +0100

    controller auth done

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -2,23 +2,20 @@ const bcrypt = require('bcryptjs/dist/bcrypt');
 const authenticateUtil = require('../../utils/authenticate.js');
 
 const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 exports.signin = async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        const user = await prisma.users.findUnique({
+        const user = await prisma.user.findUnique({
             where: {
                 email: email,
             },
-        })
+        });
 
         if (user) {
-            var passwordIsValid = bcrypt.compareSync(
-                password,
-                user.password
-            );
+            const passwordIsValid = bcrypt.compareSync(password, user.password);
 
             if (passwordIsValid) {
                 const accessToken = authenticateUtil.generateAccessToken({ id: user.id, name: user.name });
@@ -30,26 +27,39 @@ exports.signin = async (req, res) => {
         res.status(401).json({ msg: "invalid_login" });
 
     } catch (error) {
-        res.status(401).json({ msg: error.message })
+        res.status(500).json({ msg: "Internal Server Error" });
     }
 }
 
 
 exports.signup = async (req, res) => {
     try {
-        const { name, email, password, isAdmin } = req.body;
+        const { name, email, password } = req.body;
 
-        await prisma.users.create({
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                email: email,
+            },
+        });
+
+        if (existingUser) {
+            return res.status(400).json({ msg: "User already exists" });
+        }
+
+        const hashedPassword = bcrypt.hashSync(password, 8);
+
+        const newUser = await prisma.user.create({
             data: {
                 email: email,
                 name: name,
-                password: bcrypt.hashSync(password, 8),
-                isAdmin: isAdmin
+                password: hashedPassword,
             },
-        })
+        });
 
-        return this.signin(req, res);
+        const accessToken = authenticateUtil.generateAccessToken({ id: newUser.id, name: newUser.name });
+        res.status(201).json({ name: newUser.name, token: accessToken });
+
     } catch (error) {
-        res.status(401).json({ msg: error.message })
+        res.status(500).json({ msg: "Internal Server Error" });
     }
 }
