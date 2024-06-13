268a44340f6688286f6416fde883563f32a9799e
Andre Vale <andrevale_6@hotmail.com>
2024-06-13 21:36:58 +0100

    add controller getuser and updateUser

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,7 +1,10 @@
 const authRouter = require('express').Router();
 const controller = require('../../controllers/auth/auth');
+const authMiddleware = require('../../middlewares/auth');
 
 authRouter.post('/signin', controller.signin);
 authRouter.post('/signup', controller.signup);
 
+authRouter.get('/user/:id', authMiddleware, controller.getUser);
+authRouter.put('/UpdateUser/:id', authMiddleware, controller.updateUser);
 module.exports = authRouter;
