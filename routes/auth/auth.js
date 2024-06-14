0e193e201d934b6cd420e94b67da956d305e4df9
Andre Vale <andrevale_6@hotmail.com>
2024-06-14 02:25:19 +0100

    t

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,11 +1,11 @@
 const authRouter = require('express').Router();
 const controller = require('../../controllers/auth/auth');
-//const authMiddleware = require('../../middlewares/auth');
+const authMiddleware = require('../../middlewares/auth');
 
 authRouter.post('/signin', controller.signin);
 authRouter.post('/signup', controller.signup);
 
-authRouter.get('/user/:id',  controller.getUser);
-authRouter.put('/updateuser/:id',  controller.updateUser);
+authRouter.get('/user/:id', authMiddleware, controller.getUser);
+authRouter.put('/updateuser/:id', authMiddleware, controller.updateUser);
 
 module.exports = authRouter;
