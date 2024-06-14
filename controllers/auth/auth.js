38062d8b2e6c404fe51e21590bbd76c8ecbad92d
Andre Vale <andrevale_6@hotmail.com>
2024-06-14 16:42:45 +0100

    add console log

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -95,6 +95,7 @@ exports.updateUser = async (req, res) => {
             });
 
             if (existingEmailUser && existingEmailUser.id !== userId) {
+                console.error(`Error while trying to get user: ${error}`);
                 return res.status(400).json({ msg: "Email already in use by another user" });
             }
         }
@@ -105,6 +106,7 @@ exports.updateUser = async (req, res) => {
             });
 
             if (existingNifUser && existingNifUser.id !== userId) {
+                console.error(`Error while trying to get user: ${error}`);
                 return res.status(400).json({ msg: "NIF already in use by another user" });
             }
         }
